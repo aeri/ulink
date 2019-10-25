8123899b3ed94d3983cec3f40066d6989931a41d
javiermixture17 <javiermf.98@gmail.com>
2019-10-25 21:24:37 +0200

    Heroku PORT
    Co-Authored-By: NAVAL <aeri@users.noreply.github.com>

--- src/main/java/urlshortener/Application.java
+++ src/main/java/urlshortener/Application.java
@@ -9,6 +9,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
+        String ENV_PORT = System.getenv().get("PORT");
+        String ENV_DYNO = System.getenv().get("DYNO");
+        if(ENV_PORT != null && ENV_DYNO != null) {
+            System.getProperties().put("server.port", ENV_PORT);
+        }
+
         SpringApplication.run(Application.class, args);
     }
 
